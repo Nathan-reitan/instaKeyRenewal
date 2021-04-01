eab337230742eefd23732f4bc2330dcc851156f5
Nathan-reitan <nathanreitan@gmail.com>
2021-04-01 10:06:57 -0700

    initial setup, webpack, npm, starter dependencies

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+require('dotenv/config');
+const path = require('path');
+
+const clientPath = path.join(__dirname, 'client/');
+const publicPath = path.join(__dirname, 'server/public/');
+
+module.exports = {
+  resolve: {
+    extensions: ['.js']
+  },
+  entry: clientPath,
+  output: {
+    path: publicPath
+  },
+  module: {
+    
+  },
+  devtool: 'source-map',
+  devServer: {
+    contentBase: publicPath,
+    historyApiFallback: true,
+    host: '0.0.0.0',
+    port: process.env.DEV_SERVER_PORT,
+    proxy: {
+      '/api': `http://localhost:${process.env.PORT}`
+    },
+    stats: 'minimal',
+    watchContentBase: true
+  }
+};
